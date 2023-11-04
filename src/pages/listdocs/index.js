b514774dbf446d5edfdf8dafc6d8ae83cbe27eb6
kawasaki <iduchungho@gmail.com>
2023-11-04 14:22:28 +0700

    update docs

--- src/pages/listdocs/index.js
+++ src/pages/listdocs/index.js
@@ -13,29 +13,37 @@ export default function ListDocs() {
                         <td className=" w-1/5">Ver</td>
                     </tr>
                 </table> */}
-                <table class="table-auto">
-                    <thead>
-                        <tr>
-                            <th>Song</th>
-                            <th>Artist</th>
-                            <th>Year</th>
+                <table class="table-auto w-4/5 text-center">
+                    <thead className="bg-blue-400">
+                        <tr className="">
+                            <th >ID</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Version</th>
+                            <th>Link</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                            <td>Malcolm Lockyer</td>
                             <td>1961</td>
+                            <td>Malcolm Lockyer</td>
+                            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                            <td>1.0.0</td>
+                            <td>Link</td>
                         </tr>
                         <tr>
+                            <td>1972</td>
                             <td>Witchy Woman</td>
                             <td>The Eagles</td>
-                            <td>1972</td>
+                            <td>1.0.0</td>
+                            <td>Link</td>
                         </tr>
                         <tr>
+                            <td>1975</td>
                             <td>Shining Star</td>
                             <td>Earth, Wind, and Fire</td>
-                            <td>1975</td>
+                            <td>1.0.0</td>
+                            <td>Link</td>
                         </tr>
                     </tbody>
                 </table>
