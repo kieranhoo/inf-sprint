b79c7c2ea6dd0cf54b1c55838a57eb386d6c4b07
huynhbaotin172@gmail.com <huynhbaotin172@gmail.com>
2023-11-04 10:19:49 +0700

    First commit

---
+++ src/main/java/com/example/DocumentManagement/controller/DocumentController.java
@@ -0,0 +1,15 @@
+package com.example.DocumentManagement.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/document")
+@RequiredArgsConstructor
+public class DocumentController {
+
+
+}
