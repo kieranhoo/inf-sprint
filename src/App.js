73080e2559eb6de61bf971d31c9f25383176188f
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2023-11-04 10:43:55 +0700

    initial frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [file, setFile] = useState('');
+
+  // Handles input change event and updates state
+  function handleChange(event) {
+    setFile(event.target.files[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" accept="image/*" onChange={handleChange} />
+      <button>Upload to Firebase</button>
     </div>
   );
 }
