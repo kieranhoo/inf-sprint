94402372e0c5eac5ceec4d546fe491abbdf34166
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-12-03 12:17:29 +0700

    add DocumentServiceTest, DocumentControllerTest, modified Document

---
+++ server/src/test/java/com/example/DocumentManagement/controller/DocumentControllerTest.java
@@ -0,0 +1,146 @@
+package com.example.DocumentManagement.controller;
+
+import com.example.DocumentManagement.entity.DepartmentEntity;
+import com.example.DocumentManagement.entity.VersionEntity;
+import com.example.DocumentManagement.response.*;
+import com.example.DocumentManagement.service.DocumentService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import javax.swing.text.Document;
+import java.sql.Date;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+@ExtendWith(MockitoExtension.class)
+public class DocumentControllerTest {
+
+    /*
+     * TESTING STRATEGY: BLACK-BOX DECISION TABLE TESTING
+     *
+     * 1. GetAllDocuments
+     * - Input: PageIndex, PageSize
+     * - Constrain:
+     *   + Valid PageSize: 0 < PageSize < 100 (PageSize<0 -> PageSize=10; PageSize>=100 -> PageSize=100)
+     *   + Valid PageIndex: 0 < PageIndex < TotalPage in DB (PageIndex<0 -> PageIndex=1)
+     * - States:
+     *   a/ Valid PageIndex - Valid PageSize, Valid PageIndex - Invalid PageSize,
+     *   Invalid PageIndex - Valid PageSize, Invalid PageIndex - Invalid PageSize
+     *   b/ Non-empty PageResponse, PageResponse with Empty DocumentResponse in DB
+     * - Output: Response PageResponse (Non-empty, Empty)
+     *
+     * 2. GetDepartmentById
+     * - Input: DepartmentId
+     * - States:
+     *   a/ Valid DepartmentId, Invalid DepartmentId
+     *   b/ Non-empty Department, Zero Department, Empty Department in DB
+     * - Output: Response DepartmentEntity (Non-Null, Null)
+     *
+     * 3. GetAllUsersFromDepartmentId
+     * - Input: DepartmentId
+     * - States:
+     *   a/ Valid DepartmentId, Invalid DepartmentId
+     *   b/ Users in Department, No Users in Department, Zero Department in DB
+     * - Output: Response List of UsersEntity (Non-empty, Empty, Null)
+     * */
+
+
+    @InjectMocks
+    DocumentController documentController;
+    @Mock
+    DocumentService documentService;
+    @Test
+    void getDocumentById () {
+        // Given
+        String idString = "123";
+        int id = 123;
+        DepartmentEntity department = new DepartmentEntity();
+        department.setName("IT Department");
+        department.setDescription("Handles IT-related tasks");
+
+        VersionEntity version1 = new VersionEntity();
+        version1.setDocumentId(id);
+        version1.setUrl("http://example.com/document/1");
+        version1.setName("Version 1");
+        version1.setCurrentVersion(true);
+        version1.setUpdateTime(Date.valueOf("2023-11-09"));
+        version1.setNote("Initial version");
+
+        VersionEntity version2 = new VersionEntity();
+        version2.setDocumentId(id);
+        version2.setUrl("http://example.com/document/1");
+        version2.setName("Version 2");
+        version2.setCurrentVersion(false);
+        version2.setUpdateTime(Date.valueOf("2023-11-10"));
+        version2.setNote("Updated version");
+
+        List<VersionEntity> versions = Arrays.asList(version1, version2);
+
+        DocumentResponse documentResponse = new DocumentResponse();
+        documentResponse.setId(id);
+        documentResponse.setName("Mocked Document");
+        documentResponse.setDescription("Mocked Description");
+        documentResponse.setCreateTime(Date.valueOf("2023-11-10"));
+        documentResponse.setDepartment(department);
+        documentResponse.setVersions(versions);
+
+        when(documentService.getDocumentById(idString)).thenReturn(documentResponse);
+        // When
+        ResponseEntity<DocumentResponse> result = documentController.getDocumentById(idString);
+        // Then
+        Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+        Assertions.assertEquals(documentResponse, result.getBody());
+        verify(documentService).getDocumentById(idString);
+    }
+
+    @Test
+    void getAllDocuments () {
+        // Given
+        int id = 123;
+        DepartmentEntity department = new DepartmentEntity();
+        department.setName("IT Department");
+        department.setDescription("Handles IT-related tasks");
+
+        VersionEntity version1 = new VersionEntity();
+        version1.setDocumentId(id);
+        version1.setUrl("http://example.com/document/1");
+        version1.setName("Version 1");
+        version1.setCurrentVersion(true);
+        version1.setUpdateTime(Date.valueOf("2023-11-09"));
+        version1.setNote("Initial version");
+
+        VersionEntity version2 = new VersionEntity();
+        version2.setDocumentId(id);
+        version2.setUrl("http://example.com/document/1");
+        version2.setName("Version 2");
+        version2.setCurrentVersion(false);
+        version2.setUpdateTime(Date.valueOf("2023-11-10"));
+        version2.setNote("Updated version");
+
+        List<VersionEntity> versions = Arrays.asList(version1, version2);
+
+        DocumentResponse documentResponse = new DocumentResponse();
+        documentResponse.setId(id);
+        documentResponse.setName("Mocked Document");
+        documentResponse.setDescription("Mocked Description");
+        documentResponse.setCreateTime(Date.valueOf("2023-11-10"));
+        documentResponse.setDepartment(department);
+        documentResponse.setVersions(versions);
+
+        ListResponse expectedResponse = new ListResponse(Arrays.asList(documentResponse));
+        when(documentService.getAllDocuments()).thenReturn(expectedResponse);
+        // When
+        ResponseEntity<ListResponse> result = documentController.getAllDocuments();
+        // Then
+        Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+        Assertions.assertEquals(expectedResponse, result.getBody());
+        verify(documentService).getAllDocuments();
+    }
+}
---
+++ server/src/test/java/com/example/DocumentManagement/service/DocumentServiceTest.java
@@ -0,0 +1,133 @@
+package com.example.DocumentManagement.service;
+
+import com.example.DocumentManagement.entity.DepartmentEntity;
+import com.example.DocumentManagement.entity.DocumentEntity;
+import com.example.DocumentManagement.entity.VersionEntity;
+import com.example.DocumentManagement.exception.NotFoundException;
+import com.example.DocumentManagement.repository.DepartmentRepository;
+import com.example.DocumentManagement.repository.DocumentRepository;
+import com.example.DocumentManagement.repository.VersionRepository;
+import com.example.DocumentManagement.response.DocumentResponse;
+import com.example.DocumentManagement.response.ListResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.print.Doc;
+import java.sql.Date;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DocumentServiceTest {
+    @InjectMocks
+    private DocumentService documentService;
+    @Mock
+    private DocumentRepository documentRepository;
+    @Mock
+    private VersionRepository versionRepository;
+    @Mock
+    private DepartmentRepository departmentRepository;
+    @Test
+    void givenDocuments_whenGetAllDocuments_thenReturnAllDocuments() {
+        // Given
+        DepartmentEntity departmentEntity = new DepartmentEntity("Management", "Management Room");
+        DocumentEntity document1 = new DocumentEntity("Document1", "Document1 Description", Date.valueOf("2023-11-10"), false, null, "1");
+        document1.setId(1);
+        VersionEntity version1doc1 = new VersionEntity(1, "https://example.com/version/1", "Version 1.0.0", true, Date.valueOf("2023-11-10"), "Original");
+        VersionEntity version2doc1 = new VersionEntity(1, "https://example.com/version/1", "Version 1.0.1", true, Date.valueOf("2023-11-10"), "Update");
+        DocumentEntity document2 = new DocumentEntity("Document2", "Document2 Description", Date.valueOf("2023-11-10"), false, null, "1");
+        document2.setId(2);
+        VersionEntity version1doc2 = new VersionEntity(2, "https://example.com/version/2", "Version 2.0.0", true, Date.valueOf("2023-11-10"), "Original");
+        VersionEntity version2doc2 = new VersionEntity(2, "https://example.com/version/2", "Version 2.0.1", true, Date.valueOf("2023-11-10"), "Update");
+        List<DocumentEntity> documents = Arrays.asList(document1, document2);
+        List<VersionEntity> listVersion1 = Arrays.asList(version1doc1, version1doc2);
+        List<VersionEntity> listVersion2 = Arrays.asList(version2doc1, version2doc2);
+
+        DocumentResponse documentResponse1 = new DocumentResponse(document1.getId(), document1.getName(), document1.getDescription(), document1.getCreateTime(), departmentEntity, listVersion1);
+        DocumentResponse documentResponse2 = new DocumentResponse(document2.getId(), document2.getName(), document2.getDescription(), document2.getCreateTime(), departmentEntity, listVersion2);
+        ListResponse expectedResult = new ListResponse(Arrays.asList(documentResponse1, documentResponse2));
+        // Mock repository calls
+        when(documentRepository.findAllDocuments()).thenReturn(documents);
+        when(versionRepository.findByDocumentIdAndCurrentVersionTrue(1)).thenReturn(listVersion1);
+        when(versionRepository.findByDocumentIdAndCurrentVersionTrue(2)).thenReturn(listVersion2);
+        when(departmentRepository.findDepartmentById(1)).thenReturn(departmentEntity);
+
+        // When
+        ListResponse results = documentService.getAllDocuments();
+        // Then
+        Assertions.assertEquals(expectedResult.getListContent(), results.getListContent());
+        // Verify repository calls
+        verify(documentRepository).findAllDocuments();
+        verify(departmentRepository, times(2)).findDepartmentById(1);
+        verify(versionRepository).findByDocumentIdAndCurrentVersionTrue(1);
+        verify(versionRepository).findByDocumentIdAndCurrentVersionTrue(2);
+    }
+    @Test
+    void givenZeroDocuments_whenGetAllDocuments_thenReturnZeroDocuments() {
+        // Given
+
+        // Mock repository calls
+        when(documentRepository.findAllDocuments()).thenReturn(Collections.emptyList());
+
+        // When
+        ListResponse results = documentService.getAllDocuments();
+
+        // Then
+        Assertions.assertEquals(Collections.emptyList(), results.getListContent());
+        // Verify repository calls
+        verify(documentRepository).findAllDocuments();
+    }
+
+    @Test
+    void givenTrueId_whenFindDocumentById_thenReturnDocument() {
+        // Given
+        DocumentEntity expectedDocument = new DocumentEntity("Document1", "Document1 Description", Date.valueOf("2023-11-10"), false, null, "1");
+        expectedDocument.setId(1);
+        VersionEntity version1 = new VersionEntity(expectedDocument.getId(), "https://example.com/version/1", "Version 1.0.0", true, Date.valueOf("2023-11-10"), "Initial version");
+        VersionEntity version2 = new VersionEntity(expectedDocument.getId(), "https://example.com/version/2", "Version 1.0.1", false, Date.valueOf("2023-11-11"), "Update version");
+        List<VersionEntity> listVersions = Arrays.asList(version1, version2);
+        DepartmentEntity departmentEntity = new DepartmentEntity("Management", "Management Room");
+        when(documentRepository.findDocumentById(expectedDocument.getId())).thenReturn(expectedDocument);
+        when(departmentRepository.findDepartmentById(Integer.parseInt(expectedDocument.getDepartmentId()))).thenReturn(departmentEntity);
+        when(versionRepository.findByDocumentId(expectedDocument.getId())).thenReturn(listVersions);
+
+        DocumentResponse expectedDocumentResponse = new DocumentResponse(
+                expectedDocument.getId(),
+                expectedDocument.getName(),
+                expectedDocument.getDescription(),
+                expectedDocument.getCreateTime(),
+                departmentEntity,
+                listVersions
+        );
+        // When
+        DocumentResponse result = documentService.getDocumentById((expectedDocument.getId()).toString());
+        // Then
+        Assertions.assertEquals(expectedDocumentResponse, result);
+        // Verify repository calls
+        verify(documentRepository).findDocumentById(1);
+        verify(departmentRepository).findDepartmentById(1);
+        verify(versionRepository).findByDocumentId(1);
+    }
+
+    @Test
+    void givenWrongId_whenFindDocumentById_thenReturnNull() {
+        // Given
+        when(documentRepository.findDocumentById(2)).thenReturn(null);
+
+        // When
+        NotFoundException exception = Assertions.assertThrows(NotFoundException.class, () -> {
+            documentService.getDocumentById("2");
+        });
+        // Then
+        Assertions.assertEquals("Document not found!", exception.getMessage());
+        // Verify repository calls
+        verify(documentRepository).findDocumentById(2);
+    }
+}
