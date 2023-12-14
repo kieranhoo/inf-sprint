d4a32d68abb08e4355c15a7e4b38fb5d7ea70fbf
huynhbaotin172@gmail.com <huynhbaotin172@gmail.com>
2023-12-14 15:50:03 +0700

    finish authenticate service test

--- server/src/main/java/com/example/DocumentManagement/repository/TokenCategoryRepository.java
+++ server/src/main/java/com/example/DocumentManagement/repository/TokenCategoryRepository.java
@@ -1,10 +1,15 @@
 package com.example.DocumentManagement.repository;
 
 import com.example.DocumentManagement.entity.TokenCategoryEntity;
+import com.example.DocumentManagement.entity.TokenTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TokenCategoryRepository extends JpaRepository<TokenCategoryEntity, Integer> {
     Optional<TokenCategoryEntity> findByTokenCategoryName(String tokenCategoryName);
+
+    @Query(value = "SELECT * FROM token_category where token_category_name = ?", nativeQuery = true)
+    TokenCategoryEntity findCategoryByTokenCategoryName(String tokenCategoryName);
 }
